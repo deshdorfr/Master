29a8c895c40d58be4fbb976e1223af7b2f4d4404
deshdeepak Pandey <deshdeepak.pandey@resourcifi.com>
2024-11-17 21:08:13 +0530

    scheduler removed

--- Master/api/apps.py
+++ Master/api/apps.py
@@ -5,5 +5,5 @@ class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
     
-    def ready(self):
-        from . import tasks  # Import to start the scheduler
+    # def ready(self):
+    #     from . import tasks  # Import to start the scheduler
